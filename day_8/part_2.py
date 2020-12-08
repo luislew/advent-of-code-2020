184945a22eacbd0443127cecbb27ecd2815e207d
Jeremy Lewis <luislew@gmail.com>
2020-12-07 22:33:00 -0800

    fix comments, more minor cleanup

--- day_8/part_2.py
+++ day_8/part_2.py
@@ -6,21 +6,20 @@ def swap_instruction_at_idx(instructions, idx) -> bool:
     op, value = instructions[idx]
     if op == "acc":
         return False
-    else:
-        instructions[idx] = ("jmp", value) if op == "nop" else ("nop", value)
-        return True
+
+    instructions[idx] = ("jmp" if op == "nop" else "nop", value)
+    return True
 
 
 def get_value_for_fixed_instructions(instructions, visited) -> int:
     """Modify visited instructions in place, one at a time, and look for execution without infinite loop"""
     for idx in visited:
         if swap_instruction_at_idx(instructions, idx):
-            # Check if swapping instruction returns None
             accumulated_value, _, infinite_loop = calculate_value_at_infinite_loop_or_end(instructions)
+            # If swapping instruction breaks the infinite loop, return value; otherwise, swap back and continue
             if infinite_loop is False:
                 return accumulated_value
             else:
-                # Swap back in place
                 swap_instruction_at_idx(instructions, idx)
 
     raise AssertionError("No fix detected!")
