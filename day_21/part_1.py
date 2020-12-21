c3f62430227d84dabcba15dd61943135460ac4e5
Jeremy Lewis <luislew@gmail.com>
2020-12-21 00:51:20 -0500

    Minor code cleanup

--- day_21/part_1.py
+++ day_21/part_1.py
@@ -1,12 +1,11 @@
+from itertools import chain
+from operator import itemgetter
+
 from day_21 import get_allergens_to_ingredient, INGREDIENTS_AND_ALLERGENS
 
 
 if __name__ == "__main__":
     allergens_to_ingredient = get_allergens_to_ingredient()
     prohibited_ingredients = set(allergens_to_ingredient.values())
-    count = 0
-    for ingredients, _ in INGREDIENTS_AND_ALLERGENS:
-        for ingredient in ingredients:
-            if ingredient not in prohibited_ingredients:
-                count += 1
-    print(count)
+    all_ingredients = chain.from_iterable(map(itemgetter(0), INGREDIENTS_AND_ALLERGENS))
+    print(len([ingredient for ingredient in all_ingredients if ingredient not in prohibited_ingredients]))
--- day_21/part_2.py
+++ day_21/part_2.py
@@ -2,5 +2,4 @@ from day_21 import get_allergens_to_ingredient
 
 
 if __name__ == "__main__":
-    allergens_to_ingredient = get_allergens_to_ingredient()
-    print(",".join(ingredient for _, ingredient in sorted(allergens_to_ingredient.items())))
+    print(",".join(ingredient for _, ingredient in sorted(get_allergens_to_ingredient().items())))
